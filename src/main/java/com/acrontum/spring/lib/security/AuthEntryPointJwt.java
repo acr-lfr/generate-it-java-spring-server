4dc7b6fcf917e8d53ea5803ba8a3cac954e3e1b0
Rafael Fernandes <rafael.fernandes@acrontum.pt>
2022-05-06 15:23:05 +0200

    ACRDDF-73 - Creates the template following the paths on the swagger file.

--- src/main/java/com/acrontum/spring/lib/security/AuthEntryPointJwt.java
+++ src/main/java/com/acrontum/spring/lib/security/AuthEntryPointJwt.java
@@ -4,8 +4,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -14,13 +13,13 @@ import org.springframework.stereotype.Component;
  * The type Auth entry point jwt.
  */
 @Component
+@Slf4j
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
-    private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
-        logger.error("Unauthorized error: {}", authException.getMessage());
+        log.error("Unauthorized error: {}", authException.getMessage());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
     }
 }
